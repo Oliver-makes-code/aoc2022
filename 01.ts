128f94b2782819ce27514f9c8c13de56b1f9ed88
Octal <59418903+oliver-makes-code@users.noreply.github.com>
2022-12-01 16:34:37 -0600

    Split into the code I used for both parts

--- 01.ts
+++ 01.ts
@@ -1,12 +1,30 @@
-const text = await Deno.readTextFile("./01.txt")
-const textSplit = text.split("\n\n")
-const numbers: number[] = []
-for (const i of textSplit) {
-    const eachLine = i.split("\n")
-    let count = 0
-    for (const line of eachLine) count += parseInt(line)
-    numbers.push(count)
+async function part1() {
+    const text = await Deno.readTextFile("./01.txt")
+    const textSplit = text.split("\n\n")
+    let max = 0
+    for (const i of textSplit) {
+        const eachLine = i.split("\n")
+        let count = 0
+        for (const line of eachLine) count += parseInt(line)
+        if (count > max) max = count
+    }
+    console.log(max)
 }
-numbers.sort((a, b) => {return a - b})
-numbers.pop()
-console.log(numbers.pop()! + numbers.pop()! + numbers.pop()!)
+
+async function part2() {
+    const text = await Deno.readTextFile("./01.txt")
+    const textSplit = text.split("\n\n")
+    const numbers: number[] = []
+    for (const i of textSplit) {
+        const eachLine = i.split("\n")
+        let count = 0
+        for (const line of eachLine) count += parseInt(line)
+        numbers.push(count)
+    }
+    numbers.sort((a, b) => {return a - b})
+    numbers.pop()
+    console.log(numbers.pop()! + numbers.pop()! + numbers.pop()!)
+}
+
+await part1()
+await part2()
